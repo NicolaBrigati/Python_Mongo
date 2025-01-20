8165aee7e87ced81870bd205e95e429fde85cedc
Nicola Brigati <n.brigati@gmail.com>
2025-01-20 22:54:16 +0100

    Delete Python_Mongo/index.py

--- Python_Mongo/index.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-#import default connection from MongoDb
-client = MongoClient('localhost', 27017)
-db = client.pymongo
-print(client.server_info())
